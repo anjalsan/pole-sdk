02f3066e174f443dd2ea1142c4358dd907cc7759
anjal <anjalsaneen123@gmail.com>
2017-11-01 00:25:03 +0530

    notification support and package id changed

---
+++ pole_android_sdk/src/main/java/org/poletalks/sdk/pole_android_sdk/FeedbackActivity.java
@@ -0,0 +1,58 @@
+package org.poletalks.sdk.pole_android_sdk;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+import com.google.gson.JsonObject;
+import org.poletalks.sdk.pole_android_sdk.Model.LoginResponse;
+import org.poletalks.sdk.pole_android_sdk.RetrofitSupport.ApiInterface;
+import org.poletalks.sdk.pole_android_sdk.RetrofitSupport.RetrofitConfig;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    private Context context;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+        context = this;
+//        setTitle("FEEDBACK");
+
+        getdata("jdp_superadmin", "password");
+    }
+
+
+    public void getdata(String login, String pass) {
+        RetrofitConfig retrofitConfig = new RetrofitConfig(context);
+        Retrofit retro = retrofitConfig.getRetro();
+        final ApiInterface admin = retro.create(ApiInterface.class);
+
+        JsonObject abc = new JsonObject();
+        abc.addProperty("login_id", login);
+        abc.addProperty("password", pass);
+
+        Call<LoginResponse> call = admin.adminLogin(abc);
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.code() == 200) {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Toast.makeText(context, "Oops, something went wrong!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
