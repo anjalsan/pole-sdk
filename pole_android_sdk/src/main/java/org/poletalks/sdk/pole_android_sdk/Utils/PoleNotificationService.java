908b94e94a11d1b6e4463231550a799746ab978c
anjal <anjalsaneen123@gmail.com>
2017-11-01 20:25:36 +0530

    pole notification service

--- pole_android_sdk/src/main/java/org/poletalks/sdk/pole_android_sdk/Utils/PoleNotificationService.java
+++ pole_android_sdk/src/main/java/org/poletalks/sdk/pole_android_sdk/Utils/PoleNotificationService.java
@@ -5,60 +5,29 @@ import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
-
-import org.poletalks.sdk.pole_android_sdk.RetrofitSupport.ApiInterface;
-import org.poletalks.sdk.pole_android_sdk.RetrofitSupport.RetrofitConfig;
 
 import java.io.IOException;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-
 /**
- * Created by LENOVO on 04-Feb-16.
+ * Created by anjal on 11/1/17.
  */
 
+public class PoleNotificationService {
 
-public class MyInstanceIDListenerService extends FirebaseInstanceIdService {
-
-    /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is also called
-     * when the InstanceID token is initially generated, so this is where
-     * you retrieve the token.
-     */
-    // [START refresh_token]
-    @Override
-    public void onTokenRefresh() {
-//        // Get updated InstanceID token.
-//        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-//        Log.d("MyInstanceId", "Refreshed token: " + refreshedToken);
-//        // TODO: Implement this method to send any registration to your app's servers.
-//        sendRegistrationToServer(refreshedToken);
-
+    public String getToken() {
         String refreshedToken = null;
         try {
             refreshedToken = FirebaseInstanceId.getInstance().getToken("732877727331", "FCM");
+            Log.d("MyInstanceId", "sdk:Refreshed token: " + refreshedToken);
+            sendRegistrationToServer(refreshedToken);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Log.d("MyInstanceId", "sdk:Refreshed token: " + refreshedToken);
         // TODO: Implement this method to send any registration to your app's servers.
-        sendRegistrationToServer(refreshedToken);
+        return (refreshedToken);
     }
 
-
     private void sendRegistrationToServer(String refreshedToken) {
-        final SharedPreferences pref = getSharedPreferences("PolePref", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putString("fcm_token", refreshedToken);
-        editor.commit();
-
-        Log.e("fcm_token", refreshedToken);
-
 //        if (CheckNetwork.isInternetAvailable(getApplicationContext()))
 //        {
 //            RetrofitConfig retrofitConfig = new RetrofitConfig(getApplicationContext());
